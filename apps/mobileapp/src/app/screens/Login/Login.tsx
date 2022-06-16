98c0a99cb1204701a3e49b2e88e9f32785283887
Karan Kalsi <serkarankalsi@gmail.com>
2022-06-16 12:11:18 +0530

    feat(mobileapp): add navigation support

---
+++ apps/mobileapp/src/app/screens/Login/Login.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View } from 'react-native';
+import LoginStyles from './Login.styles';
+
+const Login: React.FC = () => {
+  return <View style={LoginStyles.container}></View>;
+};
+
+export default Login;
